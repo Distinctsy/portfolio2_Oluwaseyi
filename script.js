8419b52842523f445d508ce28f9771eb14e1248d
Oluwaseyi Simon <117735331+Distinctsy@users.noreply.github.com>
2023-09-10 23:46:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const day = document.querySelector(".day");
+const time = document.querySelector(".time")
+
+
+
+window.addEventListener('load',calculatetime)
+    function calculatetime (){
+        var date = new Date();
+        var daynumber = date.getDay();      
+        var daynames =["SUNDAY", "MONDAY" , "TUESDAY", "WENESDAY","THRURSDAY", "FRIDAY","SATURDAY"] 
+        var currentTimeInMilliseconds = new Date().getTime();    
+        document.querySelector(".day").innerHTML = daynames[daynumber];
+        document.querySelector(".time").innerHTML = currentTimeInMilliseconds;
+        setTimeout(calculatetime,200);
+        
+    }
